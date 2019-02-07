bb4b12433275b3b8e9c828d83096fc8c36767c76
Thomas Norris <tom.norris135@gmail.com>
2019-02-07 09:47:10 -0500

    padding works for a message less than 1024 bits

--- OneWayHash/SHA512.cpp
+++ OneWayHash/SHA512.cpp
@@ -1,6 +1,30 @@
 #include "pch.h"
 #include "SHA512.h"
+#include "OneWayHash.h"
+#include <bitset>
+
+using namespace std;
 
 void GenerateHash(string message)
 {
+	// Pad the message
+	int l = message.length();
+	int k = 896 - (l + 1);
+
+	message += "1";
+	for (auto i = 0; i < k; ++i)
+	{
+		message += "0";
+	}
+
+	auto lBinary = bitset<8>(l).to_string();
+
+	for (auto i = 0; i < 128 - lBinary.length(); ++i)
+	{
+		message += "0";
+	}
+
+	message += lBinary;
+
+	PrintToConsole(std::to_string(message.length()));
 }
