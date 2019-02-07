027bfd7d3069f33b8b30a2a1ba91dea527259cdc
Thomas Norris <tom.norris135@gmail.com>
2019-02-07 10:53:23 -0500

    outline of object

---
+++ OneWayHash/SHAMessage.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "SHAMessage.h"
+
+
+SHAMessage::Block::Group::Group()
+{
+}
+
+SHAMessage::Block::Group::~Group()
+{
+}
+
+SHAMessage::Block::Block()
+{
+}
+
+SHAMessage::Block::~Block()
+{
+}
+
+SHAMessage::SHAMessage()
+{
+}
+
+
+SHAMessage::~SHAMessage()
+{
+}
---
+++ OneWayHash/SHAMessage.h
@@ -0,0 +1,24 @@
+#ifndef SHAMESSAGE_H
+#define SHAMESSAGE_H
+
+class SHAMessage
+{
+public:
+	class Block
+	{
+	public:
+		class Group
+		{
+			Group();
+			~Group();
+		};
+
+		Block();
+		~Block();
+	};
+
+	SHAMessage();
+	~SHAMessage();
+};
+
+#endif // SHAMESSAGE_H
